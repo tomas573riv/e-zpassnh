3fca96ac00facdffc5107e1e373c878728c69371
tomas573riv <tomas573riv@outlook.com>
2025-03-22 15:34:06 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+{
+  "short_name": "e-zpassnh",
+  "name": "e-zpassnh - Expense Tracker",
+  "icons": [],
+  "start_url": "/",
+  "display": "standalone",
+  "theme_color": "#000000",
+  "background_color": "#ffffff"
+}
